b41865369c76623e12863873fbe36e60dcbb8ce2
José Lisandro Obregon <obregonjoselisandro@gmail.com>
2025-06-14 12:58:13 -0300

    add(backend/recetas): agrega el endpoint /:id y arregla /
    - se crean las funciones necesarias en la capa de la base de datos
    - se crean las funciones necesarias en la api
    - se agrega manejo de errores en las funciones anteriormente creadas

--- backend/src/db/recetas.js
+++ backend/src/db/recetas.js
@@ -1,10 +1,32 @@
 const dbClient = require('./client.js');
 
 async function getAllRecetas() {
-    const result = await dbClient.query('SELECT * FROM recetas');
-    return result.rows;
+    try {
+        const result = await dbClient.query('SELECT * FROM recetas');
+        return result.rows;
+    } catch (error) {
+        console.error('Error al obtener recetas:', error);
+        throw new Error('No se pudieron obtener las recetas');
+    }
+}
+
+async function getRecetaById(id) {
+    try {
+        if (!id) {
+            throw new TypeError('Falta el ID de la receta');
+        }
+        const result = await dbClient.query('SELECT * FROM recetas WHERE id = $1', [id]);
+        if (result.rows.length === 0) {
+            throw new Error('Receta no encontrada');
+        }
+        return result.rows[0];
+    } catch (error) {
+        console.error('Error al obtener la receta:', error);
+        throw new Error('No se pudo obtener la receta');
+    }
 }
 
 module.exports = {
-    getAllRecetas
+    getAllRecetas,
+    getRecetaById
 };
--- backend/src/routes/recetas.js
+++ backend/src/routes/recetas.js
@@ -1,10 +1,34 @@
 const express = require('express');
 const router = express.Router();
-const { getAllRecetas } = require('../db/recetas');
+const { getAllRecetas, getRecetaById } = require('../db/recetas');
 
 router.get('/', async function (req, res) {
-    const result = await getAllRecetas();
-    res.json(result);
+    try {
+        const result = await getAllRecetas();
+        res.json(result);
+    } catch (error) {
+        res.status(500).json({ error: 'Error interno del servidor' });
+    }
+});
+
+router.get('/:id', async function (req, res) {
+    const { id } = req.params;
+    try {
+        if (isNaN(id)) {
+            const error = new TypeError('El ID no es un número');
+            error.cause = 'id';
+            throw error;
+        }
+        const result = await getRecetaById(id);
+        res.json(result);
+    } catch (error) {
+        console.error('Error al procesar la petición:', error);
+        if (error instanceof TypeError)
+        {
+            return res.status(400).json({ error: `Hubo un error de tipo de dato con: ${error.cause}` });
+        }
+        res.status(404).json({ error: 'Receta no encontrada' });
+    }
 });
 
 module.exports = router;
