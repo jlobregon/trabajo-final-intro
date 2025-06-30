af4d66d83e4a86bbdaeaac199edd22658441447d
José Lisandro Obregon <obregonjoselisandro@gmail.com>
2025-06-30 00:41:08 -0300

    add(backend): implementa cors para admitir peticiones del frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const { PORT } = process.env;
 const recetasRoutes = require('./routes/recetas');
 const chefsRoutes = require('./routes/chefs');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.get('/', function (req, res) {
   res.redirect('api/v1/status');
 });
