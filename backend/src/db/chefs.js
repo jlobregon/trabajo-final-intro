6e6acec6748a82fb02abaa110d78c2243c7e73d5
Lisandro Garbarino <ligarbarino@fi.uba.ar>
2025-06-30 02:10:38 -0300

    add(backend/chefs.js): Agrega los endpoints para crear chefs y obtener un solo chef

--- backend/src/db/chefs.js
+++ backend/src/db/chefs.js
@@ -1,16 +1,24 @@
 const dbClient = require('./client.js');
 
 async function getAllChefs() {
-    const result = await dbClient.query('SELECT * FROM recetas');
+    const result = await dbClient.query('SELECT * FROM chefs');
     return result.rows;
 }
 
-async function getChef(id) {
-    const result = await dbClient.query('SELECT * FROM recetas WHERE id = $1', [id]);
+async function getChefById(id) {
+    const result = await dbClient.query('SELECT * FROM chefs WHERE id = $1', [id]);
     return result.rows[0];
 }
 
+async function createChef(chef) {
+    const { nombre, acerca_de, especialidad, localidad, imagen_url } = chef;
+    const result = await dbClient.query(
+        'INSERT INTO chefs (nombre, acerca_de, especialidad, localidad, imagen_url) VALUES ($1, $2, $3, $4, $5) RETURNING *',
+        [nombre, acerca_de, especialidad, localidad, imagen_url]
+    );
+    return result.rows[0];
+}
 module.exports = {
     getAllChefs,
-    getChef
+    getChefById
 };
