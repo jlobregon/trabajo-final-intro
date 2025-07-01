ea3c2b3a16391c80340773c0c8ed62703ccd15b0
Lisandro Garbarino <ligarbarino@fi.uba.ar>
2025-07-01 18:08:03 -0300

    add(frontend/js/index.js): Fetchea el back de chefs y establece los campos en las tarjetas de recetas

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -4,35 +4,38 @@ columnas[1] = document.getElementById('recetas-cl-2');
 columnas[2] = document.getElementById('recetas-cl-3');
 let currentIndex = 0;
 
-fetch('http://localhost:3000/api/v1/recetas')
-    .then(result => result.json())
-    .then(data => {
-        data.forEach(receta => {
-            const elementoReceta = document.createElement('div');
-            elementoReceta.classList.add('card');
+Promise.all([
+    fetch('http://localhost:3000/api/v1/recetas').then(result => result.json()),
+    fetch('http://localhost:3000/api/v1/chefs').then(result => result.json())
+])
+.then(([recetas, chefs]) => {
+    recetas.forEach(receta => {
+        const chef = chefs.find(c => c.id === receta.chef_id)
+        const elementoReceta = document.createElement('div');
+        elementoReceta.classList.add('card');
 
-            elementoReceta.innerHTML = `
-                <div class="card-image">
-                    <figure class="image is-1by1 receta-imagen">
-                    <img src="img/rata1-2.jpg" alt="Receta">
-                    </figure>
-                </div>
-                <div class="card-content">
-                    <div class="media">
-                        <div class="media-left">
-                            <figure class="image is-48x48">
-                                <img src="" alt="PFP">
-                            </figure>
-                        </div>
-                        <div class="media-content">
-                            <p class="title is-5">${receta.nombre}</p>
-                            <p class="subtitle is-6">NOMBRE DE PERFIL</p>
-                        </div>
+        elementoReceta.innerHTML = `
+            <div class="card-image">
+                <figure class="image is-1by1 receta-imagen">
+                <img src="${receta.image_url}" alt="Receta">
+                </figure>
+            </div>
+            <div class="card-content">
+                <div class="media">
+                    <div class="media-left">
+                        <figure class="image is-48x48">
+                            <img src="${chef.image_url || "img/perfil-default.png"}" alt="PFP">
+                        </figure>
+                    </div>
+                    <div class="media-content">
+                        <p class="title is-5">${receta.nombre}</p>
+                        <p class="subtitle is-6">${chef.nombre}</p>
                     </div>
                 </div>
-                `;
-            if (currentIndex === 3) { currentIndex = 0 };
-            columnas[currentIndex].appendChild(elementoReceta);
-            currentIndex++;
-        });
+            </div>
+            `;
+        if (currentIndex === 3) { currentIndex = 0 };
+        columnas[currentIndex].appendChild(elementoReceta);
+        currentIndex++;
     });
+});
