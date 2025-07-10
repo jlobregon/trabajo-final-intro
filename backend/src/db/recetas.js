f21066af0ec55c89cd4b72bfdce5dc3108d8de36
Tomas Chirino <tomasch2000@hotmail.com>
2025-07-10 17:48:49 -0300

    fix(backend/recetas): Arreglo indentación

--- backend/src/db/recetas.js
+++ backend/src/db/recetas.js
@@ -1,39 +1,42 @@
 const dbClient = require('./client.js');
 
 async function getAllRecetas() {
-    const result = await dbClient.query(`SELECT r.id, r.nombre, r.descripcion, r.categoria, r.nivel_dificultad, r.tiempo_estimado, r.imagen_url,
-                                        json_agg(
-                                            json_build_object(
-                                            'ingrediente_id', i.id,
-                                            'nombre_ingrediente', i.nombre,
-                                            'cantidad_ingredientes', ir.cantidad_ingredientes
-                                            )
-                                        ) AS ingredientes
-                                        FROM recetas r
-                                        JOIN ingredientes_recetas ir ON ir.receta_id = r.id
-                                        JOIN ingredientes i ON i.id = ir.ingrediente_id
-                                        GROUP BY r.id
-                                        ORDER BY r.id`
+    const result = await dbClient.query(
+        `SELECT r.id, r.nombre, r.descripcion, r.categoria, r.nivel_dificultad, r.tiempo_estimado, r.imagen_url,
+            json_agg(
+                json_build_object(
+                'ingrediente_id', i.id,
+                'nombre_ingrediente', i.nombre,
+                'cantidad_ingredientes', ir.cantidad_ingredientes
+                )
+            ) AS ingredientes
+        FROM recetas r
+        JOIN ingredientes_recetas ir ON ir.receta_id = r.id
+        JOIN ingredientes i ON i.id = ir.ingrediente_id
+        GROUP BY r.id
+        ORDER BY r.id`
 );
     return result.rows;
 }
 
 async function getRecetaById(id) {
-    const result = await dbClient.query(`SELECT r.id, r.nombre, r.descripcion, r.categoria, r.nivel_dificultad, r.tiempo_estimado, r.imagen_url,
-                                        json_agg(
-                                            json_build_object(
-                                            'ingrediente_id', i.id,
-                                            'nombre_ingrediente', i.nombre,
-                                            'cantidad_ingredientes', ir.cantidad_ingredientes
-                                            )
-                                        ) AS ingredientes
-                                        FROM recetas r
-                                        JOIN ingredientes_recetas ir ON ir.receta_id = r.id
-                                        JOIN ingredientes i ON i.id = ir.ingrediente_id
-                                        WHERE r.id = $1
-                                        GROUP BY r.id
-                                        ORDER BY r.id`,
-         [id]);
+    const result = await dbClient.query(
+        `SELECT r.id, r.nombre, r.descripcion, r.categoria, r.nivel_dificultad, r.tiempo_estimado, r.imagen_url,
+            json_agg(
+                json_build_object(
+                'ingrediente_id', i.id,
+                'nombre_ingrediente', i.nombre,
+                'cantidad_ingredientes', ir.cantidad_ingredientes
+                )
+            ) AS ingredientes
+        FROM recetas r
+        JOIN ingredientes_recetas ir ON ir.receta_id = r.id
+        JOIN ingredientes i ON i.id = ir.ingrediente_id
+        WHERE r.id = $1
+        GROUP BY r.id
+        ORDER BY r.id`,
+        [id]
+    );
     return result.rows[0];
 }
 
